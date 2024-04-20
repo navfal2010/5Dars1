22e5f36492ee3ab8f8c56f515379045e4a9b1dab
navfal2010 <navfal808@gmail.com>
2024-04-20 10:28:37 +0500

    done

---
+++ main.js
@@ -0,0 +1,28 @@
+let first = +prompt('Birdan 7 gacha son kiriting')
+switch (first) {
+    case 1: alert('Dushanba')
+        break
+
+    case 2: alert('Seshanba')
+        break
+
+    case 3: alert('Chorshanba')
+        break
+
+    case 4: alert('Payshanba')
+        break
+
+    case 5: alert('Juma')
+        break
+
+    case 6: alert('Shanba')
+        break
+
+    case 7: alert('Yakshanba')
+        break
+
+    default: alert('Iltimos birdan 7 gacha son kiriting')
+        break
+}
+
+
